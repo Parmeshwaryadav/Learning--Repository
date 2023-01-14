17d6149a730abd22cfdb915872d8bb80ca3c1bba
Parmeshwar yadav <yadavparmeshwar342@gmail.com>
2023-01-14 17:00:20 +0530

    class files

---
+++ Javascript_10-12-2022/10-12-2022 Basic pillers of Javascript/index.js
@@ -0,0 +1,6 @@
+const firstName = "Parmeshwar";
+const lastName = "yadav";
+const smallname = "Param";
+
+
+console.log(`my name is khan $(firstName), $(lastName)`);
---
+++ Javascript_10-12-2022/11-12-2022/practice.js
@@ -0,0 +1,44 @@
+// const bankAccount = 'p';
+
+// switch (bankAccount) {
+//     case 'manas':
+//     console.log("login by manas")
+//     break;
+//     case 'Parmeshwar':
+//     console.log('Login by parmeshwar')
+//     break;
+//     default:
+//     console.log('Person Not matched')
+//     break;
+//     default:
+//     console.log('Person Not matched')
+// }
+
+// const foo = 5;
+// switch (foo) {
+//   case 2:
+//     console.log(2);
+//     break;
+//   default:
+//     console.log('default')
+//     // fall-through
+//   case 1:
+//     console.log('1');
+// }
+
+
+// const Animal = 36;
+// // const Animal1 = 'elephant';
+
+// switch (Animal) {
+//   case 'Cow':
+//   case 36:
+//     console.log('this is 10');
+//   case 'Dog':
+//   case 'Pig':
+//     console.log('This animal is not extinct.');
+//     break;
+//   case 'Dinosaur':
+//   default:
+//     console.log('This animal is extinct.');
+// }
