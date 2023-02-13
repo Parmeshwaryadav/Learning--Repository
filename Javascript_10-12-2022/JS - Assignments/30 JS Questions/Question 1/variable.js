455d288f18e31098f9fd6721e349659aa9f9f634
Parmeshwar yadav <yadavparmeshwar342@gmail.com>
2023-02-13 23:59:35 +0530

    directory changes

---
+++ Javascript_10-12-2022/JS - Assignments/30 JS Questions/Question 1/variable.js
@@ -0,0 +1,9 @@
+// variable declaration
+const string = "Its a String";
+const boolean = true;
+const undefined =
+  // output
+  console.log(typeof string); //should be string '', or "";
+console.log(typeof boolean); //true or false;
+console.log(typeof 43); // number
+console.log(typeof itsNull); //
